b4b85d923b9165772b0f129e3144646c8390b18e
acxtrilla <cazz9584@gmail.com>
2026-03-30 14:43:21 +0100

    Fix upgrade command and add skillkit dependency

--- src/upgrade.tsx
+++ src/upgrade.tsx
@@ -1,21 +1,9 @@
-import { showFailureToast } from "@raycast/utils";
-import { useEffect } from "react";
 import { updateAllGlobalPackages } from "./lib/npm";
 
-export default function UpgradeCommand() {
-  useEffect(() => {
-    async function runUpgrade() {
-      try {
-        await updateAllGlobalPackages();
-      } catch (error) {
-        await showFailureToast(error, {
-          title: "Failed to upgrade global packages",
-        });
-      }
-    }
-
-    runUpgrade();
-  }, []);
-
-  return null;
+export default async function UpgradeCommand(): Promise<void> {
+  try {
+    await updateAllGlobalPackages();
+  } catch {
+    // updateAllGlobalPackages already shows a failure toast
+  }
 }
